6fed9c09b4ea3f177801214c6287b08773281282
David idris <didwiz83@gmail.com>
2020-04-03 21:27:20 +0100

    inital commit

---
+++ resources/js/components/WeatherApp.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="p-2 text-white mb-8">
+    <div class="places-input text-gray-800 ">
+      <input type="text" class="w-full p-2 rounded-sm" />
+    </div>
+    <div
+      class="weather-container font-sans w-128 max-w-lg overflow-hidden bg-gray-900 shadow-lg mt-4 rounded-lg"
+    >
+      <div class="current-weather flex items-center justify-between px-6 py-8">
+        <div class="flex items-center">
+          <div>
+            <div class="text-6xl font-semibold">{{ currentTemp.actual }}°C</div>
+            <div>Feels like {{ currentTemp.feels }}°C</div>
+          </div>
+          <div class="mx-5">
+            <div class="font-semibold">{{ currentTemp.summary }}</div>
+            <div>{{ location.name }}</div>
+          </div>
+        </div>
+
+        <div>
+          <canvas
+            ref="iconCurrent"
+            id="iconCurrent"
+            width="96"
+            height="96"
+          ></canvas>
+        </div>
+      </div>
+
+      <div class="future-weather text-sm bg-gray-800 px-6 py-8 overflow-hidden">
+        <div
+          v-for="(day, index) in daily"
+          :key="day.time"
+          :class="{ 'mt-8': index > 0 }"
+          v-if="index < 5"
+          class="flex items-center"
+        >
+          <div class="w-1/6 text-lg text-gray-200 uppercase">
+            {{ day.time | day }}
+          </div>
+          <div class="w-4/6 px-4 flex items-center">
+            <div>
+              <canvas
+                :id="`icon${index + 1}`"
+                :data-icon="toKebabCase(day.icon)"
+                width="24"
+                height="24"
+              ></canvas>
+            </div>
+            <div class="ml-3">{{ day.summary }}</div>
+          </div>
+          <div class="w-1/6 text-right">
+            <div>{{ Math.round(day.temperatureHigh) }}°C</div>
+            <div>{{ Math.round(day.temperatureHigh) }}°C</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.fetchData();
+  },
+  filters: {
+    day(val) {
+      const newDate = new Date(val * 1000);
+      const days = ["sun", "mon", "tue", "wed", "thur", "fri", "sat"];
+      return days[newDate.getDay()];
+    }
+  },
+  data() {
+    return {
+      currentTemp: {
+        actual: 0,
+        feels: "",
+        summary: "",
+        icon: ""
+      },
+      location: {
+        name: "Toronto, Canada",
+        lat: 43.6532,
+        lng: -79.38323
+      },
+      daily: []
+    };
+  },
+  methods: {
+    fetchData() {
+      let skycons = new Skycons({ color: "white" });
+      axios
+        .get("/api/weather", {
+          params: {
+            lat: this.location.lat,
+            lng: this.location.lng
+          }
+        })
+        .then(({ data: { currently: ct, daily } }) => {
+          this.currentTemp.actual = Math.round(ct.temperature);
+          this.currentTemp.feels = Math.round(ct.apparentTemperature);
+          this.currentTemp.summary = ct.summary;
+          this.currentTemp.icon = this.toKebabCase(ct.icon);
+
+          this.daily = daily.data;
+
+          skycons.add("iconCurrent", this.currentTemp.icon);
+          skycons.play();
+
+          this.$nextTick(() => {
+            skycons.add(
+              "icon1",
+              document.getElementById("icon1").getAttribute("data-icon")
+            );
+            skycons.add(
+              "icon2",
+              document.getElementById("icon2").getAttribute("data-icon")
+            );
+            skycons.add(
+              "icon3",
+              document.getElementById("icon3").getAttribute("data-icon")
+            );
+            skycons.add(
+              "icon4",
+              document.getElementById("icon4").getAttribute("data-icon")
+            );
+            skycons.add(
+              "icon5",
+              document.getElementById("icon5").getAttribute("data-icon")
+            );
+            skycons.play();
+          });
+        });
+    },
+    toKebabCase(string) {
+      return string.split(" ").join("-");
+    }
+  }
+};
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weather App</title>
+    <link rel="stylesheet" href="/css/main.css">
+    <script src="https://rawgithub.com/darkskyapp/skycons/master/skycons.js" defer></script>
+    <script src="/js/app.js" defer></script>
+</head>
+<body class="bg-blue-200">
+    <div id="app">
+        <div class="flex flex-row justify-center pt-16">
+            <weather-app />
+        </div>
+    </div>
+</body>
+</html>
